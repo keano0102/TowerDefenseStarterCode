3e724869a383c69c4b6788aab8dacceca24b1cea
keano0102 <keano.pittevils@student.arcadiascholen.be>
2024-03-19 15:24:50 +0100

    13th commit
    stap 6 halfweg klaar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private ConstructionSite selectedSite;
     public GameObject TowerMenu;
     private TowerMenu towerMenu;
+    public List<GameObject> Archers = new List<GameObject>();
+    public List<GameObject> Swords = new List<GameObject>();
+    public List<GameObject> Wizards = new List<GameObject>();
     // Start is called before the first frame update 
-    void Start()
+    public void SelectSite(ConstructionSite site)
     {
+        // Onthoud de geselecteerde site
+        this.selectedSite = site;
+
+        // Geef de geselecteerde site door aan het towerMenu door SetSite aan te roepen
+        towerMenu.SetSite(site);
+    }
+
+    void Start()
+    { 
         towerMenu = TowerMenu.GetComponent<TowerMenu>();
     }
 }
--- Assets/Scripts/TowerMenu.cs
+++ Assets/Scripts/TowerMenu.cs
@@ -2,12 +2,75 @@ using UnityEngine;
 using UnityEngine.UIElements;
 public class TowerMenu : MonoBehaviour
 {
+    private ConstructionSite selectedSite;
     private Button archerButton;
     private Button swordButton;
     private Button wizardButton;
     private Button updateButton;
     private Button destroyButton;
     private VisualElement root;
+    public void EvaluateMenu()
+    {
+        if (selectedSite == null)
+        {
+            // If selectedSite is null, return without enabling any buttons
+            return;
+        }
+
+        // Access the site level property of selectedSite
+        int siteLevel = (int)selectedSite.Level;
+
+        // Disable all buttons initially
+        archerButton.SetEnabled(false);
+        wizardButton.SetEnabled(false);
+        swordButton.SetEnabled(false);
+        updateButton.SetEnabled(false);
+        destroyButton.SetEnabled(false);
+
+        // Enable buttons based on site level using a switch statement
+        switch (siteLevel)
+        {
+            case 0:
+                // For site level 0, enable archer, wizard, and sword buttons
+                archerButton.SetEnabled(true);
+                wizardButton.SetEnabled(true);
+                swordButton.SetEnabled(true);
+                break;
+            case 1:
+            case 2:
+                // For site levels 1 and 2, enable update and destroy buttons
+                updateButton.SetEnabled(true);
+                destroyButton.SetEnabled(true);
+                break;
+            case 3:
+                // For site level 3, only enable the destroy button
+                destroyButton.SetEnabled(true);
+                break;
+            default:
+                // Handle any other site levels if necessary
+                break;
+        }
+    }
+    public void SetSite(ConstructionSite site)
+    {
+        // Assign the site to the selectedSite variable
+        selectedSite = site;
+
+        if (selectedSite == null)
+        {
+            // If the selected site is null, hide the menu and return
+            root.visible = false;
+            return;
+        }
+        else
+        {
+            // If the selected site is not null, make sure the menu is visible
+            root.visible = true;
+
+            // Call the EvaluateMenu method to update button visibility
+            EvaluateMenu();
+        }
+    }
     void Start()
 
     {
